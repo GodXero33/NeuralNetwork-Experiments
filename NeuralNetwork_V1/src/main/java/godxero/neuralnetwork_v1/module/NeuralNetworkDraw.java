d55082144e73152d74b4127964ba64160438ba92
GodXero <shansathish33@gmail.com>
2024-12-29 17:19:29 +0530

    "Update"

--- NeuralNetwork_V1/src/main/java/godxero/neuralnetwork_v1/module/NeuralNetworkDraw.java
+++ NeuralNetwork_V1/src/main/java/godxero/neuralnetwork_v1/module/NeuralNetworkDraw.java
@@ -5,8 +5,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 
 public class NeuralNetworkDraw {
-	private NeuralNetwork network;
-	private Vector2D mouse;
+	final private NeuralNetwork network;
+	final private Vector2D mouse;
 
 	public NeuralNetworkDraw (NeuralNetwork network) {
 		this.network = network;
@@ -24,6 +24,7 @@ public class NeuralNetworkDraw {
 		final double yInputOffset = (yInputGap - size) * 0.5;
 		final double yOutputOffset = (yOutputGap - size) * 0.5;
 		final int inputSize_2 = inputSize * 2;
+		final double halfSize = size * 0.5;
 
 		ctx.setFill(new Color(1.0, 0.0, 0.0, 1.0));
 		ctx.setStroke(new Color(0.0, 0.0, 0.0, 1.0));
@@ -31,8 +32,8 @@ public class NeuralNetworkDraw {
 		ctx.beginPath();
 
 		for (int a = 0; a < inputSize; a++) for (int b = 0; b < outputSize; b++) {
-			ctx.moveTo(size * 1.5 - width * 0.5, a * yInputGap + yInputOffset - height * 0.5 + size * 0.5);
-			ctx.lineTo(width * 0.5 - size * 1.5, b * yOutputGap + yOutputOffset - height * 0.5 + size * 0.5);
+			ctx.moveTo(size * 1.5 - width * 0.5, a * yInputGap + yInputOffset - height * 0.5 + halfSize);
+			ctx.lineTo(width * 0.5 - size * 1.5, b * yOutputGap + yOutputOffset - height * 0.5 + halfSize);
 		}
 
 		ctx.stroke();
@@ -51,6 +52,32 @@ public class NeuralNetworkDraw {
 
 		for (int y = 0; y < inputSize; y++) ctx.fillOval(nodes[y * 2], nodes[y * 2 + 1], size, size);
 		for (int y = 0; y < outputSize; y++) ctx.fillOval(nodes[inputSize_2 + y * 2], nodes[inputSize_2 + y * 2 + 1], size, size);
+
+		ctx.setFill(new Color(1.0, 0.0, 1.0, 1.0));
+
+		for (int a = 0; a < inputSize_2; a += 2) {
+			final double x = nodes[a];
+			final double y = nodes[a + 1];
+			final double mx = this.mouse.getX() - width * 0.5;
+			final double my = this.mouse.getY() - height * 0.5;
+			final double distance = (mx - x - halfSize) * (mx - x - halfSize) + (my - y - halfSize) * (my - y - halfSize);
+
+			if (distance < halfSize * halfSize) {
+				ctx.fillOval(x, y, size, size);
+			}
+		}
+
+		for (int a = 0; a < outputSize * 2; a += 2) {
+			final double x = nodes[inputSize_2 + a];
+			final double y = nodes[inputSize_2 + a + 1];
+			final double mx = this.mouse.getX() - width * 0.5;
+			final double my = this.mouse.getY() - height * 0.5;
+			final double distance = (mx - x - halfSize) * (mx - x - halfSize) + (my - y - halfSize) * (my - y - halfSize);
+
+			if (distance < halfSize * halfSize) {
+				ctx.fillOval(x, y, size, size);
+			}
+		}
 	}
 
 	public void updateMouse (double x, double y) {
